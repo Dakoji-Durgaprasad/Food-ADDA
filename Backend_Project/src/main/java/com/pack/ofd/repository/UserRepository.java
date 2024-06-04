81e0b78327628813db5ab886af8ec0790ef206f2
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-04 20:59:11 +0530

    Update UserRepository.java

--- Backend_Project/src/main/java/com/pack/ofd/repository/UserRepository.java
+++ Backend_Project/src/main/java/com/pack/ofd/repository/UserRepository.java
@@ -1,13 +1,9 @@
 package com.pack.ofd.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pack.ofd.model.User;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
-	List<User> findByEmailAndPassword(String email, String password);
 
 }
