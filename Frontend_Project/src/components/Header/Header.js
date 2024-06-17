6c27fa6591d51aa9cd53b5917dcdff7a48fb5582
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-17 08:04:11 +0530

    Update Header.js

--- Frontend_Project/src/components/Header/Header.js
+++ Frontend_Project/src/components/Header/Header.js
@@ -1,28 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
+  import React from "react";
+  import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <>
-      <nav className="navbar navbar-dark bg-primary ">
-        <div className="container-fluid">
-          <a className="navbar-brand fs-3 px-5">Food ADDA</a>
-          <div className="lg-rg bg-primary mx-2 px-2">
-            <Link to="/login">
-              <button type="button" className="btn btn-outline-light mx-2">
-               Login 
-              </button>
-            </Link>
-            <Link to="/register">
-              <button type="button" className="btn btn-outline-light mx-2">
-                Register
-              </button>
-            </Link>
+  const Header = () => {
+    return (
+      <>
+        <nav className="navbar navbar-dark bg-primary ">
+          <div className="container-fluid">
+            <a className="navbar-brand fs-3 px-5">Food ADDA</a>
+            <div className="lg-rg bg-primary mx-2 px-2">
+              <Link to="/welcome/resregister" className="btn btn-outline-light mx-2">
+                Restaurant Register 
+              </Link>
+              <Link to="/welcome/register" className="btn btn-outline-light mx-2">
+                Customer Register
+              </Link>
+              <Link to="/welcome/login" className="btn btn-outline-light mx-2">
+                Login 
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
-    </>
-  )
-}
+        </nav>
+      </>
+    )
+  }
 
-export default Header;
+  export default Header;
