94410107cf8dc9cce76c2d657eecc5ceaa50a5d5
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-17 08:09:58 +0530

    Update ViewMyFood.js

--- Frontend_Project/src/components/Restaurant/ViewMyFood.js
+++ Frontend_Project/src/components/Restaurant/ViewMyFood.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ViewMyFood = () => {
-    return(
-        <>
-        <div className="container border rounded my-5 py-5 shadow text-center ">
+  const [listOfFoods, setListOfFoods] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadFoods();
+  }, []);
+
+  const loadFoods = async () => {
+    try {
+      const result = await fetch("http://localhost:8080/viewallfoods");
+      const jsonObj = await result.json();
+      // console.log(jsonObj);
+      setListOfFoods(jsonObj);
+    } catch (error) {
+      console.error("Error loading foods:", error);
+    }
+  };
+
+  const deleteFood = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/delfood/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(listOfFoods),
+      });
+      if (response.ok) {
+        console.log("Food deleted successfully");
+      } else {
+        console.error("Failed to delete Food:", response.statusText);
+      }
+
+      loadFoods();
+    } catch (error) {
+      console.error("Error deleting Food:", error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="container border rounded my-5 py-5 shadow text-center ">
         <div className="header fs-1 text-center mb-5 bg-primary text-light rounded-top">
-            MY FOODS 
-            <div className="float-end fs-4  text-light my-3">
-                <a href="restaurantpage.html"><button type="button" className="btn btn-outline-light mx-2 px-3">BACK</button></a>
-            </div>
-            
+          MY FOODS
+          <div className="float-end fs-4  text-light my-3">
+            <Link to="/restaurant/home" className="btn btn-outline-light mx-2 px-3">
+              BACK
+            </Link>
+          </div>
         </div>
-        <table className="table">
+        {listOfFoods.length === 0 ? (
+          <p>No Food Is Added</p>
+        ) : (
+          <table className="table">
             <thead>
               <tr>
                 <th scope="col">Food</th>
@@ -23,50 +68,45 @@ const ViewMyFood = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td><img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg"  className="rounded" height="120px" width="150px"/></td>
-                <td>Pot Biryani</td>
-                <td>We have taste biryani with different styles.</td>
-                <td>Chicken</td>
-                <td>299</td>
-                <td>
+              {listOfFoods.map((food) => (
+                <tr>
+                  <td>
+                    <img
+                      src={food.foodImgUrl}
+                      className="rounded"
+                      height="120px"
+                      width="150px"
+                    />
+                  </td>
+                  <td>{food.name}</td>
+                  <td>{food.description}</td>
+                  <td>{food.categoryName}</td>
+                  <td>{food.price}</td>
+                  <td>
                     <div className="lg-rg mx-2 ">
-                        <a href="updatemyfoods.html"><button type="button" className="btn btn-outline-warning mx-2 px-3">UPDATE</button></a>
-                        <a href="viewmyfoods.html"><button type="button" className="btn btn-outline-danger mx-2 px-4">DELETE</button></a>
+                      <Link
+                        to={`/restaurant/updatefood/${food.foodId}`}
+                        className="btn btn-outline-warning mx-2 px-3 m-2"
+                      >
+                        UPDATE
+                      </Link>
+                      <button
+                        type="button"
+                        className="btn btn-danger mx-2 px-4"
+                        size="3"
+                        onClick={() => deleteFood(food.foodId)}>
+                        DELETE
+                      </button>
                     </div>
-                </td>
-              </tr>
-              <tr>
-                <td><img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="rounded" height="120px" width="150px"/></td>
-                <td>Pot Biryani</td>
-                <td>We have taste biryani with different styles.</td>
-                <td>Chicken</td>
-                <td>299</td>
-                <td>
-                    <div className="lg-rg mx-2 ">
-                        <a href="updatemyfoods.html"><button type="button" className="btn btn-outline-warning mx-2 px-3">UPDATE</button></a>
-                        <a href="viewmyfoods.html"><button type="button" className="btn btn-outline-danger mx-2 px-4">DELETE</button></a>
-                    </div>
-                </td>
-              </tr>
-              <tr>
-                <td><img src="https://cdn.pixabay.com/photo/2022/03/02/12/40/dish-7043065_1280.jpg" className="rounded" height="120px" width="150px"/></td>
-                <td>Pot Biryani</td>
-                <td>We have taste biryani with different styles.</td>
-                <td>Chicken</td>
-                <td>299</td>
-                <td>
-                    <div className="lg-rg mx-2 ">
-                        <a href="updatemyfoods.html"><button type="button" className="btn btn-outline-warning mx-2 px-3">UPDATE</button></a>
-                        <a href="viewmyfoods.html"><button type="button" className="btn btn-outline-danger mx-2 px-4">DELETE</button></a>
-                    </div>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
-        </div>
-        </>
-    )
-}
+        )}
+      </div>
+    </>
+  );
+};
 
 export default ViewMyFood;
