f1c88b57e17e14816acaba6503d019263fd1f22d
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-17 08:09:08 +0530

    Update ViewAllMyDeliveryPersons.js

--- Frontend_Project/src/components/Restaurant/ViewAllMyDeliveryPersons.js
+++ Frontend_Project/src/components/Restaurant/ViewAllMyDeliveryPersons.js
@@ -1,15 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const ViewAllMyDeliveryPersons = () => {
+  
+  const [listOfDeliveryPersons, setListOfDeliveryPersons] = useState([]);
+
+  // const { id } = useParams();
+
+  useEffect(() => {
+    loadDeliveryPersons();
+  }, []);
+
+  const loadDeliveryPersons = async () => {
+    try {
+      const result = await fetch("http://localhost:8080/alldeliverypersons");
+      const jsonObj = await result.json();
+      // console.log(jsonObj);
+      setListOfDeliveryPersons(jsonObj);
+    } catch (error) {
+      console.error("Error loading Delivery Persons:", error);
+    }
+  };
+
+  const deleteDeliveryPerson = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8080/deldeliveryperson/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(listOfDeliveryPersons),
+      });
+      if (response.ok) {
+        console.log("Delivery Person deleted successfully");
+      } else {
+        console.error("Failed to delete delivery person:", response.statusText);
+      }
+
+      loadDeliveryPersons();
+    } catch (error) {
+      console.error("Error deleting delivery person:", error.message);
+    }
+  };
+
     return(
         <div class="container border rounded my-5 py-5 shadow text-center ">
         <div class="header fs-1 text-center mb-5 bg-primary text-light rounded-top">
             ALL DELIVERY PERSONS 
             <div class="float-end fs-4  text-light my-3">
-                <a href="restaurantpage.html"><button type="button" class="btn btn-outline-light mx-2 px-3">BACK</button></a>
+                <Link to="/restaurant/home" class="btn btn-outline-light mx-2 px-3">BACK</Link>
             </div>
-            
         </div>
+        {listOfDeliveryPersons.length === 0 ? (
+          <p>No Food Is Added</p>
+        ) : (
         <table class="table">
             <thead>
               <tr>
@@ -22,18 +66,20 @@ const ViewAllMyDeliveryPersons = () => {
               </tr>
             </thead>
             <tbody>
+            {listOfDeliveryPersons.map((deliveryPerson) => (
               <tr>
-                <td>Delivery Person-1</td>
-                <td>Delivery Person-1</td>
-                <td>deliveryperson1@gmail.com</td>
-                <td>8794278</td>
-                <td>Street, City, Pincode</td>
+                <td>{deliveryPerson.firstName}</td>
+                <td>{deliveryPerson.lastName}</td>
+                <td>{deliveryPerson.emailId}</td>
+                <td>{deliveryPerson.phoneNumber}</td>
+                <td>{deliveryPerson.street}, {deliveryPerson.city}, {deliveryPerson.pinCode}</td>
                 <td>
                     <div class="lg-rg mx-2 ">
-                        <a href="viewallresdeliveryper.html"><button type="button" class="btn btn-outline-danger mx-2 px-4">DELETE</button></a>
+                      <button type="button" class="btn btn-outline-danger mx-2 px-4">DELETE</button>
                     </div>
                 </td>
               </tr>
+            ))}
               <tr>
                 <td>Delivery Person-2</td>
                 <td>Delivery Person-2</td>
@@ -54,12 +100,13 @@ const ViewAllMyDeliveryPersons = () => {
                 <td>Street, City, Pincode</td>
                 <td>
                     <div class="lg-rg mx-2 ">
-                        <a href="viewallresdeliveryper.html"><button type="button" class="btn btn-outline-danger mx-2 px-4">DELETE</button></a>
+                        <button type="button" class="btn btn-outline-danger mx-2 px-4">DELETE</button>
                     </div>
                 </td>
               </tr>
             </tbody>
           </table>
+        )}
     </div>
     )
 }
