964286c0a35db4209b05468d7bc2bdd49cc6e002
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:20:11 +0530

    Update Header.js

--- Frontend_Project/src/components/Header/Header.js
+++ Frontend_Project/src/components/Header/Header.js
@@ -6,7 +6,7 @@
       <>
         <nav className="navbar navbar-dark bg-primary ">
           <div className="container-fluid">
-            <a className="navbar-brand fs-3 px-5">Food ADDA</a>
+          <span className=" title navbar-brand fs-2 text-primary border shadow px-5">Food ADDA</span>
             <div className="lg-rg bg-primary mx-2 px-2">
               <Link to="/welcome/resregister" className="btn btn-outline-light mx-2">
                 Restaurant Register 
@@ -14,7 +14,7 @@
               <Link to="/welcome/register" className="btn btn-outline-light mx-2">
                 Customer Register
               </Link>
-              <Link to="/welcome/login" className="btn btn-outline-light mx-2">
+              <Link to="/welcome/login" className="btn btn-light text-primary mx-2 px-5">
                 Login 
               </Link>
             </div>
