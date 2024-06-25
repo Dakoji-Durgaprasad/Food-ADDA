53468a5eab4d155ea3d1592d986018d6bf85506b
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 09:57:17 +0530

    Update AdminController.java

--- Backend_Project/src/main/java/com/pack/ofd/controller/AdminController.java
+++ Backend_Project/src/main/java/com/pack/ofd/controller/AdminController.java
@@ -3,6 +3,8 @@ package com.pack.ofd.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pack.ofd.model.Admin;
 import com.pack.ofd.model.Restaurant;
+import com.pack.ofd.model.User;
 import com.pack.ofd.repository.AdminRepository;
 
 @RestController
@@ -28,5 +31,22 @@ public class AdminController {
 	@GetMapping("/admindetails")
 	List<Admin> getAllAdminDetails(){
 		return adminRepository.findAll();
+	}
+	
+	@PostMapping("/admin/login")
+	public ResponseEntity<Object> loginAdmin(@RequestBody Admin admin) {
+		
+	    if (admin.getEmailId().equals("foodadda43@gmail.com") && admin.getPassword().equals("fdad123")) {
+	        Admin defaultAdmin = new Admin();
+	        defaultAdmin.setEmailId("default@admin.com");
+	        return ResponseEntity.ok(defaultAdmin);
+	    }
+
+		Admin foundAdmin= this.adminRepository.findByEmailId(admin.getEmailId());
+		if (foundAdmin != null && foundAdmin.getPassword().equals(admin.getPassword())) {
+			return ResponseEntity.ok(foundAdmin);
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+		}
 	}
 }
