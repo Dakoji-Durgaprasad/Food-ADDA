5bcdb57b6a21124881c60f314b431ecd92e6a838
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:09:40 +0530

    Update AdminRepository.java

--- Backend_Project/src/main/java/com/pack/ofd/repository/AdminRepository.java
+++ Backend_Project/src/main/java/com/pack/ofd/repository/AdminRepository.java
@@ -3,9 +3,9 @@ package com.pack.ofd.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pack.ofd.model.Admin;
+import com.pack.ofd.model.User;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer>{
-
-	Admin findAdminByEmailIdAndPassword(String emailId, String password);
-
+	
+	 public Admin findByEmailId(String emailId);
 }
