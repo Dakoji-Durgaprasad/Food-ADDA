f0daa7ecc645153f66cab4bd6f970b75f15eeb53
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:13:14 +0530

    Delete Backend_Project/src/main/java/com/pack/ofd/service directory

--- Backend_Project/src/main/java/com/pack/ofd/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.pack.ofd.service;
-
-import com.pack.ofd.model.User;
-
-public interface UserService {
-	
-	User login(String userType, String email, String password) throws Exception;
-}
