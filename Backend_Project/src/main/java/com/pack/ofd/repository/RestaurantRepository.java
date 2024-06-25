bf6a833091b65bc98d15a1a30a66a486d7c4cea8
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:11:29 +0530

    Update RestaurantRepository.java

--- Backend_Project/src/main/java/com/pack/ofd/repository/RestaurantRepository.java
+++ Backend_Project/src/main/java/com/pack/ofd/repository/RestaurantRepository.java
@@ -3,9 +3,9 @@ package com.pack.ofd.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pack.ofd.model.Restaurant;
+import com.pack.ofd.model.User;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer>{
 
-	Restaurant findRestaurantByEmailIdAndPassword(String emailId, String password);
-
+	public Restaurant findByEmailId(String emailId);
 }
