836d61ec41203d55d14d7f5a51a7034d362b8c6c
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:00:33 +0530

    Update UserController.java

--- Backend_Project/src/main/java/com/pack/ofd/controller/UserController.java
+++ Backend_Project/src/main/java/com/pack/ofd/controller/UserController.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pack.ofd.dao.OnlineFoodDeliveryDAO;
 import com.pack.ofd.exception.UserNotFoundException;
 import com.pack.ofd.model.User;
 import com.pack.ofd.repository.UserRepository;
@@ -24,25 +24,34 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@RequestMapping("/")
-    public String home(){
-        return "Hello World!";
-    }
-	
+	public String home() {
+		return "Hello WELCOME!";
+	}
+
 	@PostMapping("/register")
 	User newUser(@RequestBody User newUser) {
 		return userRepository.save(newUser);
 	}
-	
+
 	@GetMapping("/users")
-	List<User> getAllUsers(){
+	List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
-	
+
 	@GetMapping("/user/{id}")
 	User getUserById(@PathVariable int id) {
-		return userRepository.findById(id)
-				.orElseThrow(()->new UserNotFoundException(id));
+		return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+	}
+
+	@PostMapping("/user/login")
+	public ResponseEntity<Object> loginUser(@RequestBody User user) {
+		User foundUser= this.userRepository.findByEmailId(user.getEmailId());
+		if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+			return ResponseEntity.ok(foundUser);
+		} else {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+		}
 	}
 }
