0cf593a1baee202cc64e639411fa8030dd7c14ec
Dakoji Durgaprasad <155511435+Dakoji-Durgaprasad@users.noreply.github.com>
2024-06-25 10:11:03 +0530

    Update FoodRepository.java

--- Backend_Project/src/main/java/com/pack/ofd/repository/FoodRepository.java
+++ Backend_Project/src/main/java/com/pack/ofd/repository/FoodRepository.java
@@ -1,5 +1,7 @@
 package com.pack.ofd.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,6 @@ import com.pack.ofd.model.Food;
 public interface FoodRepository extends JpaRepository<Food, Integer>{
 	
 	@Query("SELECT f FROM Food f WHERE f.restaurantId = :restaurantId")
-	Food findByRestaurantId(@Param("restaurantId")int restaurantId);
+	List<Food> findByRestaurantId(@Param("restaurantId")int restaurantId);
 
 }
